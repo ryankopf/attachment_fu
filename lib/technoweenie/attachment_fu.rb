ce9a5f561301f9864a20805a8b0c279c060ccb1a
ryankopf <ryan@ryankopf.com>
2019-04-18 23:29:54 -0500

    Updates to make compatible with Rails 5.

--- lib/technoweenie/attachment_fu.rb
+++ lib/technoweenie/attachment_fu.rb
@@ -442,6 +442,8 @@ module Technoweenie # :nodoc:
         def attachment_attributes_valid?
           [:size, :content_type].each do |attr_name|
             enum = attachment_options[attr_name]
+            Rails.logger.info "#{attr_name}: #{enum}"
+            Rails.logger.info "#{enum.to_s}"
             if Object.const_defined?(:I18n) # Rails >= 2.2
               errors.add attr_name, I18n.translate("activerecord.errors.messages.inclusion", attr_name => enum) unless enum.nil? || enum.include?(send(attr_name))
             else
