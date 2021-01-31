d7521e3b0cdc6dba9306855985d9d543c3080729
Ryan Kopf <ryankopf@users.noreply.github.com>
2021-01-30 22:32:03 -0600

    Update to the new engine format properly.

--- lib/attachment_fu/engine.rb
+++ lib/attachment_fu/engine.rb
@@ -9,10 +9,6 @@ module AttachmentFu
       # ActiveRecord::Base.send(:extend, AttachmentFu::ActMethods)
       # AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
       # FileUtils.mkdir_p AttachmentFu.tempfile_path
-      require 'geometry'
-      ActiveRecord::Base.send(:extend, AttachmentFu::ActMethods)
-      AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
-      FileUtils.mkdir_p AttachmentFu.tempfile_path
     end
   end
 end
