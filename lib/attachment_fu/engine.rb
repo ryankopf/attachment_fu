8ea1c408f6e11409fa421221b17a17602da6a34c
ryankopf <git@ryankopf.com>
2023-01-29 02:08:02 -0600

    Updating stuff.

--- lib/attachment_fu/engine.rb
+++ lib/attachment_fu/engine.rb
@@ -2,7 +2,7 @@ module AttachmentFu
   class Engine < ::Rails::Engine
     #isolate_namespace AttachmentFu
     # Mimic old vendored plugin behavior, attachment_fu/lib is autoloaded.
-    config.autoload_paths << File.expand_path("..", __FILE__)
+    #config.autoload_paths << File.expand_path("..", __FILE__)
 
   end
 end
